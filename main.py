b8c4349eb21ee3ac515ffb536843a04a3c1fbbea
niljay16 <njsfernandez@addu.edu.ph>
2024-12-04 14:39:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,9 +10,13 @@ if st.session_state.page == "main":
     st.title("DATA STRUCTURE AND ALGORITHM MOCK EXAM 2024")
 
     # Display the list of names in batches
-    st.subheader("List of Passer...")
+    st.subheader("List of Passers...")
+
+    # Batch 1
     batch_1 = [
-      
-"1. Fernandez, Nilou Jay"
-    
+        "1. Fernandez, Nilou Jay"
     ]
+
+    # Display the list of names in the batch
+    for name in batch_1:
+        st.write(name)
