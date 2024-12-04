776187de5b9a8849cec32e9eaac69d47500220bd
niljay16 <njsfernandez@addu.edu.ph>
2024-12-04 15:05:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ if st.session_state.page == "main":
     st.title("DATA STRUCTURE AND ALGORITHM MOCK EXAM 2024")
 
     # Display the list of names in batches
-    st.subheader("List of Passers... uploading 98%")
+    st.subheader("List of Passers... uploading 99%")
 
 
     # Batch 1
