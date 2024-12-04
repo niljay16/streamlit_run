2df6992575b102d1476d20a3312d76aac9d16d73
niljay16 <njsfernandez@addu.edu.ph>
2024-12-04 15:15:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,23 +7,20 @@ if "page" not in st.session_state:
 # Page content for "main"
 if st.session_state.page == "main":
     # Title of the application
-  
-    
     st.title("DATA STRUCTURE AND ALGORITHM MOCK EXAM 2024")
 
     # Display the list of names in batches
-    st.subheader("List of Passers... loading.."
-                )
-
+    st.subheader("List of Passers.")
 
     # Batch 1
-   batch_1 = [
+    batch_1 = [
         "Agohob", "Albores", "Ampo", "Baguio", "Capiña", "Celeste",
         "Diaz", "Escano", "Etulle", "Itang", "Jose", "Librea", "Lim", "Lumbab",
         "Mariano", "Mendez", "Pahila", "Palamos", "Pasaol", "Perral", "Regidor",
         "Retardo", "Ricaborda", "Sampiano", "Tandingan", "Ubas", "Vasquez", "Ybañez"
     ]
+
     st.subheader("To God Be the Glory")
-    # Display the list of names in the batch
-    for name in batch_1:
-        st.write(name)
+    # Display the list of names with numbering
+    for idx, name in enumerate(batch_1, start=1):
+        st.write(f"{idx}. {name}")
